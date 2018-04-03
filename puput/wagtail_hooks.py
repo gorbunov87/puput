91354f576c5477ceb91d4275b116bc3be3d77757
gorbunov87 <gorbunovm155@gmail.com>
2018-04-03 22:10:36 +0000

    Adding support for Django 2.0 and Wagtail 2.0 (#152)

--- puput/wagtail_hooks.py
+++ puput/wagtail_hooks.py
@@ -1,8 +1,62 @@
-from django.utils.html import format_html
-from django.conf import settings
-from wagtail.wagtailcore import hooks
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler, BlockElementHandler
+from wagtail.core import hooks
 
 
-def editor_css():
-    return format_html('<link rel="stylesheet" href="{}puput/css/puput_admin_editor.css">'.format(settings.STATIC_URL))
-hooks.register('insert_editor_css', editor_css)
+@hooks.register('register_rich_text_features')
+def register_blockquote_feature(features):
+    """
+    Registering the `blockquote` feature, which uses the `blockquote` Draft.js block type,
+    and is stored as HTML with a `<blockquote>` tag.
+    """
+    feature_name = 'blockquote'
+    type_ = 'blockquote'
+    tag = 'blockquote'
+
+    control = {
+        'type': type_,
+        'label': '❝',
+        'description': 'Quote',
+        'element': 'blockquote',
+    }
+
+    features.register_editor_plugin(
+        'draftail',
+        feature_name,
+        draftail_features.BlockFeature(control)
+    )
+
+    features.register_converter_rule(
+        'contentstate',
+        feature_name,
+        {
+            'from_database_format': {tag: BlockElementHandler(type_)},
+            'to_database_format': {'block_map': {type_: tag}},
+        }
+    )
+    features.default_features.append(feature_name)
+
+
+@hooks.register('register_rich_text_features')
+def register_codeline_feature(features):
+    feature_name = 'Code Line'
+    type_ = 'CODE'
+    tag = 'code'
+
+    control = {
+        'type': type_,
+        'label': '>_',
+        'description': 'Code Line',
+    }
+
+    features.register_editor_plugin(
+        'draftail', feature_name, draftail_features.InlineStyleFeature(control)
+    )
+
+    db_conversion = {
+        'from_database_format': {tag: InlineStyleElementHandler(type_)},
+        'to_database_format': {'style_map': {type_: tag}},
+    }
+
+    features.register_converter_rule('contentstate', feature_name, db_conversion)
+    features.default_features.append(feature_name)
